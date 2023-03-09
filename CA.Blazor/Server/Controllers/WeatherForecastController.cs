ba8d038145eb1402119a0129f9afe26574e21d68
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2023-03-09 19:17:14 +0100

    Sistemato dimensioni pdfviewer

--- CA.Blazor/Server/Controllers/WeatherForecastController.cs
+++ CA.Blazor/Server/Controllers/WeatherForecastController.cs
@@ -1,4 +1,3 @@
-using CA.Blazor.Shared;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CA.Blazor.Server.Controllers
@@ -7,28 +6,28 @@ namespace CA.Blazor.Server.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    //    private static readonly string[] Summaries = new[]
+    //    {
+    //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    //};
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    //    private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+    //    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    //    {
+    //        _logger = logger;
+    //    }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+    //    [HttpGet]
+    //    public IEnumerable<WeatherForecast> Get()
+    //    {
+    //        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //        {
+    //            Date = DateTime.Now.AddDays(index),
+    //            TemperatureC = Random.Shared.Next(-20, 55),
+    //            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    //        })
+    //        .ToArray();
+    //    }
     }
 }
